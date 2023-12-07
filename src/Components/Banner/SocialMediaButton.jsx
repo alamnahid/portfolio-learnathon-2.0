3e09aa3aea377f32433391376061c7bbd050b98d
alamnahid <nahidalam13765@gmail.com>
2023-12-07 16:29:28 +0600

    complete banner with anchor tag

--- src/Components/Banner/SocialMediaButton.jsx
+++ src/Components/Banner/SocialMediaButton.jsx
@@ -8,21 +8,34 @@ import { RiContactsFill } from "react-icons/ri";
 
 
 const SocialMediaButton = () => {
-  
+  const handleEmailClick = () => {
+    window.location.href = 'mailto:nahidalam13765@gmail.com';
+  };
+
+  const handleCallClick = () => {
+    window.location.href = 'tel:+8801796281914';
+  };
 
   return (
     <div className='mt-12 flex justify-center items-center gap-5'>
 
+    <a href="https://bd.linkedin.com/in/nahid-alam-518b431b2" target="_blank" rel="noreferrer">
     <div className='w-[3rem] h-[3rem] flex justify-center items-center rounded-[50%] bg-black shadow-sm cursor-pointer hover:bg-green-800 shadow-white'>
         <FaLinkedin className='text-xl text-white'/>
     </div>
+    </a>
+    <a href="https://github.com/alamnahid" target="_blank" rel="noreferrer">
     <div className='w-[3rem] h-[3rem] flex justify-center items-center rounded-[50%] bg-black shadow-sm cursor-pointer hover:bg-green-800 shadow-white'>
         <FaGithub className='text-xl text-white'/>
     </div>
-    <div className='w-[3rem] h-[3rem] flex justify-center items-center rounded-[50%] bg-black shadow-sm cursor-pointer hover:bg-green-800 shadow-white'>
+    </a>
+
+
+    
+    <div onClick={handleEmailClick} className='w-[3rem] h-[3rem] flex justify-center items-center rounded-[50%] bg-black shadow-sm cursor-pointer hover:bg-green-800 shadow-white'>
         <MdEmail className='text-xl text-white'/>
     </div>
-    <div className='w-[3rem] h-[3rem] flex justify-center items-center rounded-[50%] bg-black shadow-sm cursor-pointer hover:bg-green-800 shadow-white'>
+    <div onClick={handleCallClick} className='w-[3rem] h-[3rem] flex justify-center items-center rounded-[50%] bg-black shadow-sm cursor-pointer hover:bg-green-800 shadow-white'>
         <RiContactsFill className='text-xl text-white'/>
     </div>
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-scroll";
 
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     return (
         <div className="z-50">
             <div className="navbar bg-gray-950 px-[8%] fixed z-50">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <div onClick={()=>setOpen(!open)} tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ${open? '' : 'hidden'}`}>
                         <li> <Link className="text-lg hover:text-[#27AE60] cursor-pointer"  to="about" smooth={true} duration={1000}>About</Link></li>
                         <li> <Link className="text-lg hover:text-[#27AE60] cursor-pointer"  to="skills" smooth={true} duration={1000}>Skills</Link></li>
                         <li> <Link className="text-lg hover:text-[#27AE60] cursor-pointer"  to="projects" smooth={true} duration={1000}>Projects</Link></li>
